b5b3b836ae2337241f39c3bd7dacd3d548fae9f4
jay-p-b-7span <jay.p.b@7span.com>
2023-03-01 17:10:35 +0530

    SSOKSOD

---
+++ src/histoire.setup.js
@@ -0,0 +1,4 @@
+import { defineSetupVue3 } from "@histoire/plugin-vue";
+
+import "./assets/main.css";
+export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {});
---
+++ src/stories/Counter.story.vue
@@ -0,0 +1,7 @@
+<template>
+  <Story>
+    <h2 class="text-green-500 text-xl">Counter Story</h2>
+
+    <!-- <h4 class="text-xl text-blue-600">Footer Section</h4> -->
+  </Story>
+</template>
