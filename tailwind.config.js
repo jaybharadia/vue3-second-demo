65b97a352e5e7ea2a6ec446d3ee6111209615668
jay-p-b-7span <jay.p.b@7span.com>
2023-03-06 15:22:41 +0530

    require check done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+console.log("colors-->", colors);
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
