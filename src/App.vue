b1e5486fc0f3a3a8a062151d296b3c975d3e6301
jay-p-b-7span <jay.p.b@7span.com>
2023-03-09 11:32:55 +0530

    CHecking eol statement handling by git

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+
+console.log("inside app vue ");
 </script>
 
 <template>
