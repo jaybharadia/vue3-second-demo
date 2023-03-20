5effd95b713b0d2ec6069a57f05e0842e71c10af
jay-p-b-7span <jay.p.b@7span.com>
2023-03-20 11:49:39 +0530

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,43 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+<script>
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+export default {
+  data() {
+    return {
+      date: null,
+    };
+  },
+  components: {
+    VueDatePicker,
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <!-- <header> -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <VueDatePicker
+      v-model="date"
+      position="center"
+      :dark="true"
+    ></VueDatePicker>
+  </div>
+  <!-- </header> -->
 
-      <h3 class="text-cyan-500 font-mono text-xl">TAILWIND APPLIED</h3>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+:root {
+  --primary-color: #e0067a;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.dp__cell_inner.dp__pointer.dp__active_date {
+  border-radius: 50%;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.dp__theme_dark {
+  --dp-primary-color: var(--primary-color);
+  --dp-icon-color: var(--primary-color);
 }
 </style>
