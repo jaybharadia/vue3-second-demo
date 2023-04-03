af3b6983089300e08d843ee59012fb8a3f6ebe88
jay-p-b-7span <jay.p.b@7span.com>
2023-04-03 14:36:55 +0530

    Checking history mode server deployment

---
+++ src/components/user/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>User Component</h2>
+    <h4>User ID -> {{ id }}</h4>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat
+      laborum quisquam! In beatae sunt sit! Commodi voluptas adipisci nulla
+      maxime aut doloremque possimus ipsam. Accusantium ullam veniam eos minus
+      eaque autem optio tempore hic eligendi mollitia saepe, voluptatem
+      consequatur perferendis consectetur doloremque laudantium molestias
+      cupiditate odit harum eius commodi.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+};
+</script>
+
+<style scoped></style>
