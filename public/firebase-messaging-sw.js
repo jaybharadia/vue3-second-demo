88a5ddb2d2288f59cc4fb78ce0d6daf18ef361f9
jay-p-b-7span <jay.p.b@7span.com>
2023-04-03 19:25:34 +0530

    Uasda

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,49 +7,50 @@ importScripts(
   "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
 
-if (firebase.messaging.isSupported()) {
-  console.log("inside Firebase mesaging is Supported merthod ....");
-  // Initialize the Firebase app in the service worker by passing in
-  // your app's Firebase config object.
-  // https://firebase.google.com/docs/web/setup#config-object
-  firebase.initializeApp({
-    apiKey: "AIzaSyBvD5Pdw7xPvmgmd9IAIi1cjKuzx6EJDX0",
-    authDomain: "dating-demo-c61ce.firebaseapp.com",
-    projectId: "dating-demo-c61ce",
-    storageBucket: "dating-demo-c61ce.appspot.com",
-    messagingSenderId: "200243273967",
-    appId: "1:200243273967:web:0b4c048dbee50926d9fb1a",
-    measurementId: "G-K50VJ6B578",
-  });
+console.log("inside firebase service worker js ");
+// if (firebase.messaging.isSupported()) {
+console.log("inside Firebase mesaging is Supported merthod ....");
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyBvD5Pdw7xPvmgmd9IAIi1cjKuzx6EJDX0",
+  authDomain: "dating-demo-c61ce.firebaseapp.com",
+  projectId: "dating-demo-c61ce",
+  storageBucket: "dating-demo-c61ce.appspot.com",
+  messagingSenderId: "200243273967",
+  appId: "1:200243273967:web:0b4c048dbee50926d9fb1a",
+  measurementId: "G-K50VJ6B578",
+});
 
-  // Retrieve an instance of Firebase Messaging so that it can handle background
-  // messages.
-  const messaging = firebase.messaging();
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
 
-  messaging.onBackgroundMessage((payload) => {
-    console.log(
-      "[firebase-messaging-sw.js] Received background message ",
-      payload
-    );
-    // Customize notification here
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-      icon: "/logo.svg",
-    };
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/logo.svg",
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
-} else {
-  // Support for IOS Safari. Only foreground notification will be received. no background service since it is not supported.
-  self.addEventListener("push", ({ notification }) => {
-    if (self.registration && self.registration.showNotification) {
-      self.registration.showNotification(notification.title, {
-        body: notification.body,
-        icon: "/logo.svg",
-      });
-    } else {
-      alert(notification.title);
-    }
-  });
-}
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+// } else {
+//   // Support for IOS Safari. Only foreground notification will be received. no background service since it is not supported.
+//   self.addEventListener("push", ({ notification }) => {
+//     if (self.registration && self.registration.showNotification) {
+//       self.registration.showNotification(notification.title, {
+//         body: notification.body,
+//         icon: "/logo.svg",
+//       });
+//     } else {
+//       alert(notification.title);
+//     }
+//   });
+// }
