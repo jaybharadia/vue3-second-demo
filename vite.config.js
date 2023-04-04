7ed8729a594a5762dcb5cb4d5f8a0e3b1284e503
jay-p-b-7span <jay.p.b@7span.com>
2023-04-04 17:38:36 +0530

    Updated version

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    host: "app.vue3-demo.com",
+    port: 4040,
+  },
+});
