5a47948f4d592b85571312339764ddb9655bb6af
jaybharadia <jay.p.bharadia@gmail.com>
2023-05-19 17:13:33 +0545

    Checkin pwa

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       base: "/",
       srcDir: "src",
       filename: "sw.ts",
+      registerType: 'autoUpdate',
       includeAssets: ["/favicon.png"],
       strategies: "injectManifest",
       manifest: {
