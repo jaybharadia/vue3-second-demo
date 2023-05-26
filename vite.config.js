96e50017d9d443e64a0c5ddd176fc8a778cdbcdb
jaybharadia <jay.p.bharadia@gmail.com>
2023-05-26 11:25:38 +0545

    Updated version

--- vite.config.js
+++ vite.config.js
@@ -6,47 +6,46 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      base: "/",
-      srcDir: "src",
-      // filename: "sw.ts",
-      registerType: 'autoUpdate',
-      includeAssets: ["/favicon.png"],
-      strategies: "generateSW",
-      manifest: {
-        name: "DateDish",
-        short_name: "DateDish",
-        theme_color: "#E0067A",
-        start_url: "/",
-        description: "DateDish app",
-        display: "standalone",
-        background_color: "#000",
-        icons: [
-          {
-            src: "/pwa/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/pwa/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [
+        vue(),
+        VitePWA({
+            base: "/",
+            srcDir: "src",
+            // filename: "sw.ts",
+            includeAssets: ["/favicon.png"],
+            strategies: "generateSW",
+            manifest: {
+                name: "DateDish",
+                short_name: "DateDish",
+                theme_color: "#E0067A",
+                start_url: "/",
+                description: "DateDish app",
+                display: "standalone",
+                background_color: "#000",
+                icons: [
+                    {
+                        src: "/pwa/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/pwa/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/pwa/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                ],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
 });
