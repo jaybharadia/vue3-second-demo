a3dd92745e11a219e0f8cb30d44fa67979c2727a
jaybharadia <jay.p.bharadia@gmail.com>
2023-06-13 10:45:11 +0530

    Checking done

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,132 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+
+import { useVirtualList, useToggle } from "@vueuse/core";
+import { computed } from "vue";
+
+const [isEven, toggle] = useToggle();
+const allItems = Array.from(Array(9999).keys());
+const filteredList = computed(() =>
+    allItems.filter((i) => (isEven.value ? i % 2 === 0 : i % 2 === 1))
+);
+
+const { list, containerProps, wrapperProps } = useVirtualList(filteredList, {
+    itemHeight: 22,
+});
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <header>
+        <div style="width: 400px">
+            <p>Showing {{ isEven ? "even" : "odd" }} items</p>
+            <button @click="toggle">Toggle Even/Odd</button>
+            <div v-bind="containerProps" style="height: 300px">
+                <div v-bind="wrapperProps">
+                    <!-- <div
+                        v-for="item in list"
+                        :key="item.index"
+                        style="height: 22px"
+                    >
+                        Row: {{ item.data }}
+                    </div> -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+                    <select v-bind="wrapperProps">
+                        <option
+                            v-for="item in list"
+                            :key="item.index"
+                            style="heigth: 22px"
+                            :value="item.data"
+                        >
+                            Row : {{ item.data }}
+                        </option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <img
+            alt="Vue logo"
+            class="logo"
+            src="@/assets/logo.svg"
+            width="125"
+            height="125"
+        />
 
-      <h3 class="text-cyan-500 font-mono text-xl">TAILWIND APPLIED</h3>
+        <!-- <div class="wrapper">
+            <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+            <h3 class="text-cyan-500 font-mono text-xl">TAILWIND APPLIED</h3>
 
-  <RouterView />
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div> -->
+    </header>
+
+    <!-- <RouterView /> -->
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    line-height: 1.5;
+    max-height: 100vh;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+    color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+    background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
-  border: 0;
+    border: 0;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+    nav {
+        text-align: left;
+        margin-left: -1rem;
+        font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+        padding: 1rem 0;
+        margin-top: 1rem;
+    }
 }
 </style>
