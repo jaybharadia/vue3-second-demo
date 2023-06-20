9dac57fb68e43628859b8d4bb997f8c3940ef12e
jaybharadia <jay.p.bharadia@gmail.com>
2023-06-20 15:38:07 +0530

    checking done

---
+++ src/components/ChatWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <chat-window
+            :current-user-id="currentUserId"
+            :rooms="rooms"
+            :messages="messages"
+        />
+    </div>
+</template>
+
+<script>
+import ChatWindow from "vue-advanced-chat";
+
+export default {
+    components: {
+        ChatWindow,
+    },
+    data() {
+        return {
+            rooms: [],
+            messages: [],
+            currentUserId: 1234,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
