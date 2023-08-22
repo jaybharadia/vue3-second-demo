8199ee215693a28d4b29b6b3a9464b9c2c990a28
jaybharadia <jay.p.bharadia@gmail.com>
2023-08-22 20:50:16 +0530

    Updates

---
+++ src/components/accordion.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="accordion p-4 bg-gray-100 border divide-y divide-black">
+        <details class="details">
+            <summary>Can i hire Dedicated Node.js developers?</summary>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, enim
+            dicta? Deleniti numquam quae dolorem est nisi, veritatis enim
+            suscipit, eaque optio deserunt quidem cum doloribus, aspernatur
+            provident ut? Ratione fugiat consectetur exercitationem omnis
+            architecto! Corporis, facilis accusamus iste dolorem, culpa modi
+            ratione consectetur ex cumque incidunt architecto vero inventore!
+        </details>
+
+        <details>
+            <summary>Can i hire Dedicated Magento developers?</summary>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, enim
+            dicta? Deleniti numquam quae dolorem est nisi, veritatis enim
+            suscipit, eaque optio deserunt quidem cum doloribus, aspernatur
+            provident ut? Ratione fugiat consectetur exercitationem omnis
+            architecto! Corporis, facilis accusamus iste dolorem, culpa modi
+            ratione consectetur ex cumque incidunt architecto vero inventore!
+        </details>
+
+        <details>
+            <summary>Can i hire Dedicated PHP developers?</summary>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, enim
+            dicta? Deleniti numquam quae dolorem est nisi, veritatis enim
+            suscipit, eaque optio deserunt quidem cum doloribus, aspernatur
+            provident ut? Ratione fugiat consectetur exercitationem omnis
+            architecto! Corporis, facilis accusamus iste dolorem, culpa modi
+            ratione consectetur ex cumque incidunt architecto vero inventore!
+        </details>
+
+        <details>
+            <summary>Can i hire Dedicated Vue.js developers?</summary>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, enim
+            dicta? Deleniti numquam quae dolorem est nisi, veritatis enim
+            suscipit, eaque optio deserunt quidem cum doloribus, aspernatur
+            provident ut? Ratione fugiat consectetur exercitationem omnis
+            architecto! Corporis, facilis accusamus iste dolorem, culpa modi
+            ratione consectetur ex cumque incidunt architecto vero inventore!
+        </details>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.accordion {
+    details {
+        @apply p-4;
+    }
+}
+</style>
