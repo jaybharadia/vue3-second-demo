e23ab887bfa53a22c98308b60f07615915c9bb36
jaybharadia <jay.p.bharadia@gmail.com>
2023-08-31 09:50:34 +0530

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import CountryFlag from "vue-country-flag-next";
+
 import Accordion from "./components/accordion.vue";
 </script>
 
 <template>
-    <Accordion />
+    <!-- <Accordion /> -->
+    <country-flag country="hr" size="normal" />
 </template>
