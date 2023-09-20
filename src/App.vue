15d02af8f08b7884af3e9dc74d7d173c1f859631
jaybharadia <jay.p.bharadia@gmail.com>
2023-09-20 16:38:12 +0530

    Handle puchse button handler done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-    <div>Welcome to facbook pixel world {{ $options.version }}</div>
+    <div>
+        Welcome to facbook pixel world {{ $options.version }}
+        <button @click="handlePurchase">Purchase</button>
+    </div>
 </template>
 
 <script>
 import { version } from "../package.json";
 export default {
     version,
+    methods: {
+        handlePurchase() {
+            console.log("inside handle purchase");
+            window.fbq("track", "Purchase", { currency: "USD", value: 30.0 });
+        },
+    },
 };
 </script>
