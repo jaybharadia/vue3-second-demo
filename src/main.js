503a6b2671aa069f761f335594d54b35dd2adffb
jaybharadia <jay.p.bharadia@gmail.com>
2023-09-28 16:59:39 +0530

    checking done

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import router from "./router";
 
 import "./assets/style/main.css";
 
+import vueForm from "@7span/vue-form";
+
 const app = createApp(App);
 
+app.use(vueForm);
 app.use(createPinia());
 app.use(router);
 
