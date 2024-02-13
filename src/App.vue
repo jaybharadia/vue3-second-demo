fcafa018918fa0ede06be65f036f2989e9173bbb
jaybharadia <jay.p.bharadia@gmail.com>
2024-02-13 20:39:42 +0530

    vite vue devtools

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
-import Accordion from "./components/accordion.vue";
+// import Accordion from "./components/accordion.vue";
 </script>
 
 <template>
-    <Accordion />
+    <!-- <Accordion /> -->
+    <li v-for="(_, index) in 5">
+        <div class="group/1212 is-published">
+            This is Parent div
+            <div class="hidden group-hover/1212:block">Published</div>
+        </div>
+    </li>
 </template>
